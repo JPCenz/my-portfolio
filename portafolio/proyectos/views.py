3674ea876592020c3bc8bb26d3ae2f69f1c962e3
JPCenz <p.cenzano5@gmail.com>
2022-12-08 12:08:10 -0500

    agrego LoginRequiredMixin a vista para crear proyecto

--- portafolio/proyectos/views.py
+++ portafolio/proyectos/views.py
@@ -1,16 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from django.views.generic import CreateView,ListView
+from django.views.generic import CreateView, ListView
 from django.views import View
 from .models import Project
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 def index(request):
-    return render(request,template_name="index.html")
+    return render(request, template_name="index.html")
 
-class ProjectCreateView(CreateView):
+
+class ProjectCreateView(LoginRequiredMixin,CreateView):
     model = Project
     template_name = "./create.html"
-    fields= ["title","tags","photo_url","github_url","description"]
+    fields = ["title", "tags", "photo_url", "github_url", "description"]
+
     def post(self, request, *args, **kwargs):
         """
         Handle POST requests: instantiate a form instance with the passed
@@ -19,12 +23,12 @@ class ProjectCreateView(CreateView):
         form = self.get_form()
         if form.is_valid():
             form.save()
-            return redirect('portfolio')
+            return redirect("portfolio")
         else:
             print("EROOR FORM")
             return self.form_invalid(form)
 
+
 class ProjectListView(ListView):
     model = Project
     template_name = "./portfolio.html"
-
